d1edd0b167f53e5248c4f1d435fb32a6c51a67de
Tyler Wolter <twolter@epic.com>
2026-07-05 17:22:29 -0500

    Add state changes for mode

--- note-taker/src/layouts/Toolbar.jsx
+++ note-taker/src/layouts/Toolbar.jsx
@@ -1,10 +1,27 @@
 import { useState } from 'react'
+import { Button } from 'react-bootstrap'
 
 function Toolbar() {
 
+  const [isNoteMode, setIsNoteMode] = useState(true);
+  const [annotationState, setAnnotationState] = useState("");
+
+  function changeMode() {
+    setIsNoteMode(!isNoteMode);
+  }
+
   return (
     <>
-      <h2>Toolbar</h2>
+      <div id="mode-buttons">
+        <Button disabled={isNoteMode} onClick={changeMode}>Notes</Button>
+        <Button disabled={!isNoteMode} onClick={changeMode}>Quiz</Button>
+      </div>
+      <div id="state-buttons">
+        <Button>Definition</Button>
+        <Button>Fact</Button>
+        <Button>Example</Button>
+        <Button>Clear</Button>
+      </div>
     </>
   )
 }
--- note-taker/src/layouts/Toolbar.test.jsx
+++ note-taker/src/layouts/Toolbar.test.jsx
@@ -1,9 +1,43 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Toolbar from './Toolbar';
 
 describe('Toolbar Component', () => {
-  it('renders toolbar', () => {
+  it('renders toolbar with expected buttons', () => {
     render(<Toolbar />);
-    expect(screen.getByText('Toolbar')).toBeInTheDocument();
+
+    // Define mode buttons
+    const noteButton = screen.getByRole('button', { name: /notes/i });
+    const quizButton = screen.getByRole('button', { name: /quiz/i });
+
+    // Define state buttons
+    const definitionButton = screen.getByRole('button', { name: /definition/i });
+    const factButton = screen.getByRole('button', { name: /fact/i });
+    const exampleButton = screen.getByRole('button', { name: /example/i });
+
+    // Ensure the buttons exist
+    expect(noteButton).toBeInTheDocument();
+    expect(quizButton).toBeInTheDocument();
+    expect(definitionButton).toBeInTheDocument();
+    expect(factButton).toBeInTheDocument();
+    expect(exampleButton).toBeInTheDocument();
+  });
+
+  it('check initial mode button states and click', async () => {
+    render(<Toolbar />);
+
+    // Define mode buttons
+    const noteButton = screen.getByRole('button', { name: /notes/i });
+    const quizButton = screen.getByRole('button', { name: /quiz/i });
+
+    // Note is active
+    expect(noteButton.disabled).toBe(true);
+    expect(quizButton.disabled).toBe(false);
+    
+    // Test state change
+    await userEvent.click(quizButton);
+    expect(noteButton.disabled).toBe(false);
+    expect(quizButton.disabled).toBe(true);
+
   });
 });
